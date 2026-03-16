d89fd34790807da796c1b550a29bcbf8a78d383d
Laras Kurniadesi <114553727+criftarea@users.noreply.github.com>
2026-03-16 15:51:02 +0800

    Add files via upload

---
+++ modul5/larasmenghitung.php
@@ -0,0 +1,11 @@
+<?php
+
+$nilaiMatematika= 100;
+$nilaiBahasa= 75;
+$nilaiAgama= 90;
+
+$rataRata = ($nilaiMatematika + $nilaiBahasa + $nilaiAgama) / 3;
+
+echo "Rata-rata " , $rataRata ;
+
+?>
---
+++ modul5/larasvariabel.php
@@ -0,0 +1,12 @@
+<?php
+
+$nama = " Laras Kurniadesi "; 
+$umur = " 15 ";
+$kelas = " X ";
+$jurusan = " Rekayasa Perangkat Lunak ";
+$alamat = " Banjarbaru ";
+
+echo " Halo semua! Perkenalkan nama saya <b>$nama</b> saya berumur <b>$umur</b> tahun 
+saya sekarang berada di kelas <b>$kelas</b> di jurusan <b>$jurusan</b> dan alamat saya berada di <b>$alamat</b>.";
+
+?>
